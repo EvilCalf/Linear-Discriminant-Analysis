d2b46f474456c058932dd1514a44e719fc754fd4
EvilCalf <xujiading1997@163.com>
2019-09-16 14:25:34 +0800

    modify linear regression to pluralisty

--- linear regression/main.py
+++ linear regression/main.py
@@ -1,113 +1,41 @@
 import numpy as np
-import pylab
+
+#learningRate学习率，Loopnum迭代次数
 
 
-def compute_error(b, m, data):
-
-    totalError = 0
-    #Two ways to implement this
-    #first way
-    # for i in range(0,len(data)):
-    #     x = data[i,0]
-    #     y = data[i,1]
-    #
-    #     totalError += (y-(m*x+b))**2
-
-    #second way
-    x = data[:, 0]
-    y = data[:, 1]
-    totalError = (y - m * x - b)**2
-    totalError = np.sum(totalError, axis=0)
-
-    return totalError / float(len(data))
-
-
-def optimizer(data, starting_b, starting_m, learning_rate, num_iter):
-    b = starting_b
-    m = starting_m
-
-    #gradient descent
-    for i in range(num_iter):
-        #update b and m with the new more accurate b and m by performing
-        # thie gradient step
-        b, m = compute_gradient(b, m, data, learning_rate)
-        if i % 100 == 0:
-            print("iter {0}:error={1}".format(i, compute_error(b, m, data)))
-    return [b, m]
+def liner_Regression(data_x, data_y, learningRate, Loopnum):
+    Weight = np.ones(shape=(1, data_x.shape[1]))
+    #全为1.0的array，和data_x.shape[1]相同大小当前是3
+    baise = np.array([[1]]) #b
+    for num in range(Loopnum):
+        WXPlusB = np.dot(data_x, Weight.T) + baise
+        # 求出当前权值下得出的预测值y
+        loss = np.dot((data_y - WXPlusB).T, data_y - WXPlusB) / data_y.shape[0]
+        #loss就是实际值与预测值的差的平均数
+        """
+        求矩阵导数，得到新的变化率w和b，b的求法也就是相当于矩阵乘全1的那一列
+        """
+        w_gradient = -(2 / data_x.shape[0]) * np.dot((data_y - WXPlusB).T, data_x)
+        baise_gradient = -2 * np.dot(
+            (data_y - WXPlusB).T,
+            np.ones(shape=[data_x.shape[0], 1])) / data_x.shape[0]
+        #得到新的权值
+        Weight = Weight - learningRate * w_gradient
+        baise = baise - learningRate * baise_gradient
+        if num % 50 == 0:
+            print(loss)  #每迭代50次输出一次loss
+    return (Weight, baise)
 
 
-def compute_gradient(b_current, m_current, data, learning_rate):
-
-    b_gradient = 0
-    m_gradient = 0
-
-    N = float(len(data))
-    #Two ways to implement this
-    #first way
-    # for i in range(0,len(data)):
-    #     x = data[i,0]
-    #     y = data[i,1]
-    #
-    #     #computing partial derivations of our error function
-    #     #b_gradient = -(2/N)*sum((y-(m*x+b))^2)
-    #     #m_gradient = -(2/N)*sum(x*(y-(m*x+b))^2)
-    #     b_gradient += -(2/N)*(y-((m_current*x)+b_current))
-    #     m_gradient += -(2/N) * x * (y-((m_current*x)+b_current))
-
-    #Vectorization implementation
-    x = data[:, 0]
-    y = data[:, 1]
-    b_gradient = -(2 / N) * (y - m_current * x - b_current)
-    b_gradient = np.sum(b_gradient, axis=0)
-    m_gradient = -(2 / N) * x * (y - m_current * x - b_current)
-    m_gradient = np.sum(m_gradient, axis=0)
-    #update our b and m values using out partial derivations
-
-    new_b = b_current - (learning_rate * b_gradient)
-    new_m = m_current - (learning_rate * m_gradient)
-    return [new_b, new_m]
-
-
-def plot_data(data, b, m):
-
-    #plottting
-    x = data[:, 0]
-    y = data[:, 1]
-    y_predict = m * x + b
-    pylab.plot(x, y, 'o')
-    pylab.plot(x, y_predict, 'k-')
-    pylab.show()
-
-
-def Linear_regression():
-    # get train data
-    data = np.loadtxt('data.csv', delimiter=',')
-
-    #define hyperparamters
-    #learning_rate is used for update gradient
-    #defint the number that will iteration
-    # define  y =mx+b
-    learning_rate = 0.001
-    initial_b = 0.0
-    initial_m = 0.0
-    num_iter = 1000
-
-    #train model
-    #print b m error
-    print ("initial variables:\n initial_b = {0}\n intial_m = {1}\n error of begin = {2} \n"\
-        .format(initial_b,initial_m,compute_error(initial_b,initial_m,data)))
-
-    #optimizing b and m
-    [b, m] = optimizer(data, initial_b, initial_m, learning_rate, num_iter)
-
-    #print final b m error
-    print("final formula parmaters:\n b = {1}\n m={2}\n error of end = {3} \n".
-          format(num_iter, b, m, compute_error(b, m, data)))
-
-    #plot result
-    plot_data(data, b, m)
-
-
-if __name__ == '__main__':
-
-    Linear_regression()
+if __name__ == "__main__":
+    data_x = np.random.normal(0, 10, [5, 3]) #3*5的矩阵 5个array
+    """
+    numpy.random.normal(loc=0,scale=1e-2,size=shape)
+    参数loc(float)：正态分布的均值，对应着这个分布的中心。loc=0说明这一个以Y轴为对称轴的正态分布，
+    参数scale(float)：正态分布的标准差，对应分布的宽度，scale越大，正态分布的曲线越矮胖，scale越小，曲线越高瘦。
+    参数size(int 或者整数元组)：输出的值赋在shape里，默认为None。
+    """
+    Weights = np.array([[3, 4, 6]])# array行向量
+    data_y = np.dot(data_x, Weights.T) + 5
+    res = liner_Regression(data_x, data_y, learningRate=0.001, Loopnum=5000)
+    print(res[0], res[1])
