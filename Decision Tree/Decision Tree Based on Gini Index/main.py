03ef4fbc4062a73a4c7055a0ae3e37fa67d9f42a
EvilCalf <xujiading1997@163.com>
2019-09-17 11:15:04 +0800

    modify some notes

--- Decision Tree/Decision Tree Based on Gini Index/main.py
+++ Decision Tree/Decision Tree Based on Gini Index/main.py
@@ -5,38 +5,38 @@ data_file_encode = "utf-8"
 with open("D:\MyProject\机器学习\data\watermelon_2.csv",
           mode='r',
           encoding=data_file_encode) as data_file:
-    df = pd.read_csv(data_file)
+    DataSet = pd.read_csv(data_file)
 
 import DT as decision_tree
-# dicision tree visualization using pydotplus.graphviz
+# 划分训练集和测试集
 index_train = [0, 1, 2, 5, 6, 9, 13, 14, 15, 16]
 
-df_train = df.iloc[index_train]
-df_test = df.drop(index_train)
+DataSet_train = DataSet.iloc(index_train)
+DataSet_test = DataSet.drop(index_train)
 
 # generate a full tree
-root = decision_tree.TreeGenerate(df_train)
+root = decision_tree.TreeGenerate(DataSet_train)
 decision_tree.DrawPNG(root, "decision_tree_full.png")
 print("accuracy of full tree: %.3f" %
-      decision_tree.PredictAccuracy(root, df_test))
+      decision_tree.PredictAccuracy(root, DataSet_test))
 
 # pre-purning
-root = decision_tree.PrePurn(df_train, df_test)
+root = decision_tree.PrePurn(DataSet_train, DataSet_test)
 decision_tree.DrawPNG(root, "decision_tree_pre.png")
 print("accuracy of pre-purning tree: %.3f" %
-      decision_tree.PredictAccuracy(root, df_test))
+      decision_tree.PredictAccuracy(root, DataSet_test))
 
 # # post-puring
-root = decision_tree.TreeGenerate(df_train)
-decision_tree.PostPurn(root, df_test)
+root = decision_tree.TreeGenerate(DataSet_train)
+decision_tree.PostPurn(root, DataSet_test)
 decision_tree.DrawPNG(root, "decision_tree_post.png")
 print("accuracy of post-purning tree: %.3f" %
-      decision_tree.PredictAccuracy(root, df_test))
+      decision_tree.PredictAccuracy(root, DataSet_test))
 
 # print the accuracy
 # k-folds cross prediction
 accuracy_scores = []
-n = len(df.index)
+n = len(DataSet.index)
 k = 5
 for i in range(k):
     m = int(n / k)
@@ -44,19 +44,19 @@ for i in range(k):
     for j in range(i * m, i * m + m):
         test.append(j)
 
-    df_train = df.drop(test)
-    df_test = df.iloc[test]
-    root = decision_tree.TreeGenerate(df_train)  # generate the tree
-    decision_tree.PostPurn(root, df_test)  # post-purning
+    DataSet_train = DataSet.drop(test)
+    DataSet_test = DataSet.iloc[test]
+    root = decision_tree.TreeGenerate(DataSet_train)  # generate the tree
+    decision_tree.PostPurn(root, DataSet_test)  # post-purning
 
     # test the accuracy
     pred_true = 0
-    for i in df_test.index:
-        label = decision_tree.Predict(root, df[df.index == i])
-        if label == df_test[df_test.columns[-1]][i]:
+    for i in DataSet_test.index:
+        label = decision_tree.Predict(root, DataSet[DataSet.index == i])
+        if label == DataSet_test[DataSet_test.columns[-1]][i]:
             pred_true += 1
 
-    accuracy = pred_true / len(df_test.index)
+    accuracy = pred_true / len(DataSet_test.index)
     accuracy_scores.append(accuracy)
 
 # print the prediction accuracy result
--- Decision Tree/Decision Trees Based on Information Entropy/main.py
+++ Decision Tree/Decision Trees Based on Information Entropy/main.py
@@ -5,20 +5,17 @@ data_file_encode = "utf-8"  # the watermelon_3.csv is file codec type
 with open("D:\MyProject\机器学习\data\watermelon_3.csv",
           mode='r',
           encoding=data_file_encode) as data_file:
-    df = pd.read_csv(data_file)
+    DateSet = pd.read_csv(data_file)
 
 import DT as decision_tree
 
-root = decision_tree.TreeGenerate(df)
-
-# df = df.drop(['密度','含糖率'], 1)
-# df = df.drop(['色泽','根蒂','敲声','纹理','脐部','触感'], 1)
+root = decision_tree.TreeGenerate(DateSet)
 
 accuracy_scores = []
 
 # k-folds cross prediction
 
-n = len(df.index)
+n = len(DateSet.index)
 k = 5
 for i in range(k):
     m = int(n / k)
@@ -26,18 +23,18 @@ for i in range(k):
     for j in range(i * m, i * m + m):
         test.append(j)
 
-    df_train = df.drop(test)
-    df_test = df.iloc[test]
-    root = decision_tree.TreeGenerate(df_train)  # generate the tree
+    DateSet_train = DateSet.drop(test)
+    DateSet_test = DateSet.iloc[test]
+    root = decision_tree.TreeGenerate(DateSet_train)  # generate the tree
 
     # test the accuracy
     pred_true = 0
-    for i in df_test.index:
-        label = decision_tree.Predict(root, df[df.index == i])
-        if label == df_test[df_test.columns[-1]][i]:
+    for i in DateSet_test.index:
+        label = decision_tree.Predict(root, DateSet[DateSet.index == i])
+        if label == DateSet_test[DateSet_test.columns[-1]][i]:
             pred_true += 1
 
-    accuracy = pred_true / len(df_test.index)
+    accuracy = pred_true / len(DateSet_test.index)
     accuracy_scores.append(accuracy)
 
 # print the prediction accuracy result
@@ -49,6 +46,6 @@ for i in range(k):
 print("\naverage accuracy: %.3f" % (accuracy_sum / k))
 
 # dicision tree visualization using pydotplus.graphviz
-root = decision_tree.TreeGenerate(df)
+root = decision_tree.TreeGenerate(DateSet)
 
 decision_tree.DrawPNG(root, "Decision Trees Based on Information Entropy.png")
